72457f6fa74f5114f5fb924a9fe347c98a1a4d12
ncb6206 <ncb6206@gmail.com>
2023-09-06 13:39:24 +0900

    Refactor : 에러페이지 수정

--- src/pages/errorPage.tsx
+++ src/pages/errorPage.tsx
@@ -1,16 +1,29 @@
+import styled from "@emotion/styled";
 import * as React from "react";
-import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error = useRouteError();
-
   return (
     <React.Fragment>
       <div id="errorPage">
-        <h1>이런!</h1>
-        <p>죄송합니다. 예상치 못한 오류가 발생하였습니다.</p>
-        <p>{isRouteErrorResponse(error) && (error.statusText || error.data)}</p>
+        <H1>이런!</H1>
+        <P>죄송합니다. 예상치 못한 오류가 발생하였습니다.</P>
+        <P>좌측 상단의 로고를 눌러 홈화면으로 돌아가세요.</P>
       </div>
     </React.Fragment>
   );
 }
+
+const H1 = styled.h1`
+  text-align: center;
+  font-size: 6rem;
+  margin: 15px 0;
+  font-family: "Black Han Sans", sans-serif;
+  color: #57648c;
+`;
+
+const P = styled.p`
+  text-align: center;
+  font-size: 3rem;
+  margin: 15px 0;
+  font-family: "Black Han Sans", sans-serif;
+`;
